3a2b41d1b7747c0f6a02ac2ca0956a8e5055d699
dastgddg11wregds <samandarkomiljonov24@gmail.com>
2023-05-26 21:23:42 +0500

    Js 1-dars
    Yukland

---
+++ js/main.js
@@ -0,0 +1,76 @@
+/*console.log(4+(4*4)/4);*/
+/*document.write("Body oyansida ko'rinaman!");*/
+/*alert("Alert oynasida ko'rinaman!");*/
+
+/*let x = 2;*/
+/*let y = 5;*/
+/*x = 10;*/
+/*y = 15;*/
+/*let x=2,y=10,z=15;*/
+/*document.write(x+y+z);*/
+/*let salom = "#f1f1f1";*/
+/*document.write(salom);*/
+
+/* Data type in JS */
+/*let soz = "JavaScript";*/     /* string */
+/*let raqam = 45;*/             /* number */
+/*document.write(typeof raqam);*/
+
+/* Math in JS */
+/*let x = 5;*/
+/* x+=1; */   /* x=5+1 */
+/* x-=1; */   /* x=5-1 */
+/* x*=5; */   /* x=5*5 */
+/* x/=4; */   /* x=5/4 */
+/* let qoldiq = x%9; */
+/* let daraja = x**3; */      /* 8*8*8 */
+/* ++x; */
+/* --x; */
+/* x++; */
+/* --x; */
+/*document.write(5 + "matn");*/
+
+/*
+if => agar; else => u holda;
+if => agar; else if => yoki bo'lmasa; else => u holda;
+*/
+/*if ("salom" == "salom"){
+    console.log("Ular teng!");
+}
+else{
+    console.log("Ular teng emas!");
+}*/
+/*if (45 == 44){
+    console.log("Sonlar teng!")
+}
+else{
+    console.log("Sonlar teng emas!");
+}*/
+/*if (45 === 45){
+    console.log("Ushbu raqamlar teng!");
+}
+else if (45 === "45"){
+    console.log("Ushbu raqamlar endi teng!");
+}
+else{
+    console.log("Raqamlarning tengi yo'q!");
+}*/
+
+let a = 10;
+let b = 46;
+let c = 49;
+let d = 55;
+
+/*if (a == 10 && b == 46 && c == 49 && d == 55){
+    console.log("Bu shart ishlaydi, agar hamma qiymatlari to'g'ri bo'lsa");
+}
+else{
+    console.log("Bu shart ishlaydi, agar qiymatlardan kamida bittasi xato bo'lsa");
+}*/
+if (a == 11 || b == 47 || c == 48 || d == 55){
+    console.log("Bu shart ishlaydi, agar kamida bitta qiymat to'g'ri bo'lsa");
+}
+else{
+    console.log("Bu shart ishlaydi, agar hamma shartlar xato bo'lsa");
+}
+
